81595c5aa6d34bbd5222118f8cb14ebdfb638d1c
Franklin Tranié <franklin.tranie@gmail.com>
2026-06-24 20:07:25 +0200

    fix(cli): rebrand version string to Splice

--- source/version_string.d
+++ source/version_string.d
@@ -4,4 +4,4 @@ import std.format;
 
 debug { enum isDebug = " (DEBUG)"; } else { enum isDebug = ""; }
 
-enum versionStr = format!"Sideloader, compiled locally with %s on %s at %s%s"(__VENDOR__, __DATE__, __TIME__, isDebug);
+enum versionStr = format!"Splice, compiled locally with %s on %s at %s%s"(__VENDOR__, __DATE__, __TIME__, isDebug);
